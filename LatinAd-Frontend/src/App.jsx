7992fbe5d741f222cc1ca9d4ec54a5c8dbe3cd04
AgusRomero1404 <romeroagustin1404@gmail.com>
2024-05-04 19:27:21 -0300

    Declaracion de Fetchs
    Se declaran todos las llamadas a la api rest. en el documento callApi

--- LatinAd-Frontend/src/App.jsx
+++ LatinAd-Frontend/src/App.jsx
@@ -2,12 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import  { getListar } from './linker/CallApi'
+import  { crearPantalla, getListar } from './linker/CallApi'
 
 function App() {
   const [count, setCount] = useState(0)
-  
-  getListar()
+
   return (
     <>
      <h1>
--- LatinAd-Frontend/src/linker/CallApi.jsx
+++ LatinAd-Frontend/src/linker/CallApi.jsx
@@ -1,20 +1,80 @@
-import axios from 'axios';
+import axios from "axios";
 
-const urlBase = "https://challenge-front-7fw1.onrender.com"
+const urlBase = "https://challenge-front-7fw1.onrender.com";
 
-export async function getListar(){
-    try {
-        const response = await axios.get(`${urlBase}/display`, {
-            params: {
-                pageSize: 10,
-                offset: 0
-            },
-            headers: {
-                'Authorization': "Bearer MTUtcm9tZXJvYWd1c3RpbjE0MDRAZ21haWwuY29t"
-            }
-        });
-        console.log(response.data);
-    } catch (error) {
-        console.error(error);
-    }
+export async function postLogin(body) {
+  try {
+    const response = await axios.post(`${urlBase}/login`, body);
+    return response;
+  } catch (error) {
+    console.log("el error del post es", error);
+  }
+}
+
+export async function getListar(pageSize, offset, token) {
+  try {
+    const response = await axios.get(`${urlBase}/display`, {
+      params: {
+        pageSize: pageSize,
+        offset: offset,
+      },
+      headers: {
+        Authorization: "Bearer MTUtcm9tZXJvYWd1c3RpbjE0MDRAZ21haWwuY29t",
+      },
+    });
+    console.log(response.data);
+    return response;
+  } catch (error) {
+    console.error("El error en listar es: ", error);
+  }
+}
+
+export async function getDisplay(id, token) {
+  try {
+    const response = await axios.get(`${urlBase}/display/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  } catch (error) {}
+}
+
+export async function postPantalla(body, token) {
+  try {
+    const response = await axios.post(`${urlBase}/display`, body, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    console.log(response.data);
+    return response;
+  } catch (error) {
+    console.log("el error del post es", error);
+  }
+}
+
+export async function editPantalla(body, token, id) {
+  try {
+    const response = await axios.put(`${urlBase}/display/${id}`, body, {
+      headers: {
+        Authorization: `Bearer${token}`,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log("el error del put es", error);
+  }
+}
+
+export async function deletePantalla(id, token) {
+  try {
+    const response = await axios.delete(`${urlBase}/display/${id}`, {
+      headers: {
+        Authorization: `Bearer${token}`,
+      },
+    });
+  } catch (error) {
+    console.log("error al borrar: ", error);
+  }
 }
